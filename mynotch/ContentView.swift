add99319ad9545a725a8c62f70e977911672be5f
Srijan76-code <srijanpatel911@gmail.com>
2026-09-11 20:41:13 +0530

    Initial Commit

---
+++ mynotch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mynotch
+//
+//  Created by Apple on 11/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mynotch/mynotchApp.swift
@@ -0,0 +1,17 @@
+//
+//  mynotchApp.swift
+//  mynotch
+//
+//  Created by Apple on 11/09/26.
+//
+
+import SwiftUI
+
+@main
+struct mynotchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
